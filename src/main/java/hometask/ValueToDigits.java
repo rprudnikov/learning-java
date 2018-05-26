26a6cc7b52529af9b77ebd86bcb3b6321d76055c
Roman <rprudnikov@playtika.com>
2018-05-26 22:09:18 +0300

    added ValueToDigits

--- src/main/java/hometask/ValueToDigits.java
+++ src/main/java/hometask/ValueToDigits.java
@@ -3,7 +3,6 @@ package hometask;
 public class ValueToDigits {
     public static void main(String[] args) {
         int value=45685;
-
         System.out.println(value/10000);
         System.out.println(value/1000%10);
         System.out.println(value/100%10);
